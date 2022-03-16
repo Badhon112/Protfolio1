bf68dc452881a962454f4186181cca056badaac6
Badhon <biswasbadhon896@gmail.com>
2022-03-16 21:36:50 +0600

    Update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from "react";
+import About from "./container/About/About";
+import Footer from "./container/Footer/Footer";
+import Header from "./container/Header/Header";
+import Skills from "./container/Skills/Skills";
+import Testimonial from "./container/Testimonial/Testimonial";
 
 export default function App() {
   return (
-    <div className='min-h-screen bg-slate-300'>App</div>
-  )
+    <div className="min-h-screen bg-slate-300">
+      <About />
+      <Footer />
+      <Header />
+      <Skills />
+      <Testimonial />
+    </div>
+  );
 }
